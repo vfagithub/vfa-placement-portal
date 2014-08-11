ccdb57353cfebaef92c09815d996aa96bef26c86
Jason Tarre <github@ventureforamerica.org>
2014-08-11 14:05:49 -0400

    Courtesy of Forrest Miller. error related to laravel upgrade to 4.1 while portal currently running on 4.0 version.

--- www/vfamatching.dev/app/models/User.php
+++ www/vfamatching.dev/app/models/User.php
@@ -73,4 +73,19 @@ class User extends BaseModel implements UserInterface, RemindableInterface {
     {
         return array('Fellow','Hiring Manager', 'Admin');
     }
+    
+    public function getRememberToken() 
+	{ 
+		return $this->remember_token; 
+	} 
+
+	public function setRememberToken($value) 
+	{ 
+		$this->remember_token = $value; 
+	} 
+
+	public function getRememberTokenName() 
+	{ 
+		return 'remember_token'; 
+	}
 }
