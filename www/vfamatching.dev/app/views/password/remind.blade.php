03c2afaf550a8c45f66469f83a8971fbdcfc0d00
Jason Tarre <github@ventureforamerica.org>
2014-09-14 13:19:46 -0400

    Added Password controller, migration table, and views for resetting and sending reset email to allow users to reset their own password

---
+++ www/vfamatching.dev/app/views/password/remind.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.default')
+
+@section('header')
+Password Reset
+@stop
+
+@section('content')
+
+@if (Session::has('error'))
+  {{ trans(Session::get('reason')) }}
+@elseif (Session::has('success'))
+  An email with the password reset has been sent.
+@endif
+ 
+{{ Form::open(array('route' => 'password.request')) }}
+ 
+  <p>{{ Form::label('email', 'Email') }}
+  {{ Form::text('email') }}</p>
+ 
+  <p>{{ Form::submit('Submit') }}</p>
+ 
+{{ Form::close() }}
+
+@stop
